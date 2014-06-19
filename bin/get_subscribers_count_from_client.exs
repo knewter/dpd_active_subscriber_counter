f1e9f812648d20ab9e8c853060cf64990b2a5c83
Josh Adams <josh@isotope11.com>
2014-06-19 13:13:59 -0500

    Update for Elixir 0.14

--- bin/get_subscribers_count_from_client.exs
+++ bin/get_subscribers_count_from_client.exs
@@ -1,9 +1,11 @@
+alias DpdActiveSubscriberCounter.Client.ActiveSubscribers
+
 storefront_id = System.get_env("GETDPD_STORE_ID")
 username = System.get_env("GETDPD_USERNAME")
 password = System.get_env("GETDPD_API_KEY")
 
 {:ok, active_subscribers} = DpdActiveSubscriberCounter.client(storefront_id, username, password)
-active = "active subscribers: #{active_subscribers.total_count}"
-monthly_rev = "monthly revenue: #{active_subscribers.monthly_revenue}"
-yearly_rev = "yearly revenue: #{active_subscribers.yearly_revenue}"
+active = "active subscribers: #{ActiveSubscribers.total_count(active_subscribers)}"
+monthly_rev = "monthly revenue: #{ActiveSubscribers.monthly_revenue(active_subscribers)}"
+yearly_rev = "yearly revenue: #{ActiveSubscribers.yearly_revenue(active_subscribers)}"
 IO.puts "#{active} | #{monthly_rev} | #{yearly_rev}"
--- lib/dpd_active_subscriber_counter/client.ex
+++ lib/dpd_active_subscriber_counter/client.ex
@@ -1,49 +1,54 @@
 defmodule DpdActiveSubscriberCounter.Client do
   alias DpdClient.DPD, as: DPD
 
-  defrecord ActiveSubscribers, subscribers: [] do
-    def total_count(record) do
-      Enum.count(record.subscribers)
+  defmodule ActiveSubscribers do
+    defstruct subscribers: []
+
+    def total_count(struct) do
+      Enum.count(struct.subscribers)
     end
 
-    def monthly_revenue(record) do
-      record.total_count * subscription_cost
+    def monthly_revenue(struct) do
+      total_count(struct) * subscription_cost
     end
 
-    def yearly_revenue(record) do
-      record.monthly_revenue * 12
+    def yearly_revenue(struct) do
+      monthly_revenue(struct) * 12
     end
 
     defp subscription_cost do
       9
     end
   end
-  defrecord Subscriber, first_name: "", last_name: "", terms: "", next_invoice: "", email: "", status: "inactive", trial: true do
-    def active?(record) do
-      record.has_next_invoice? && (record.status == "ACTIVE") && !record.trial
+
+  defmodule Subscriber do
+   defstruct first_name: "", last_name: "", terms: "", next_invoice: "", email: "", status: "inactive", trial: true
+
+    def active?(subscriber) do
+      has_next_invoice?(subscriber) && (subscriber.status == "ACTIVE") && !subscriber.trial
     end
 
-    def has_next_invoice?(record) do
-      record.next_invoice != "" && record.next_invoice != nil
+    def has_next_invoice?(subscriber) do
+      subscriber.next_invoice != "" && subscriber.next_invoice != nil
     end
   end
 
   def get_active_subscribers(storefront_id, username, password) do
     subs = DPD.subscribers(storefront_id, username, password)
-    active_subscribers_record = ActiveSubscribers.new(subscribers: get_active_subscribers(subs))
-    {:ok, active_subscribers_record}
+    active_subscribers_struct = %ActiveSubscribers{subscribers: get_active_subscribers(subs)}
+    {:ok, active_subscribers_struct}
   end
 
   defp get_active_subscribers(subs) do
-    get_subscribers(subs) |> Enum.filter(fn(x) -> x.active? end)
+    get_subscribers(subs) |> Enum.filter(fn(x) -> Subscriber.active?(x) end)
   end
 
   defp get_subscribers(subs) do
-    Enum.map(subs, fn(data) -> Subscriber.new(attributes_from_sub(data)) end)
+    Enum.map(subs, fn(data) -> subscriber_for(data) end)
   end
 
-  defp attributes_from_sub(sub) do
-    [
+  defp subscriber_for(sub) do
+    %Subscriber{
       terms: "no clue",
       next_invoice: sub["subscription"]["next_payment_at"],
       first_name: sub["username"],
@@ -51,6 +56,6 @@ defmodule DpdActiveSubscriberCounter.Client do
       email: sub["username"],
       status: sub["subscription"]["status"],
       trial: sub["subscription"]["period"] == 0
-    ]
+    }
   end
 end
